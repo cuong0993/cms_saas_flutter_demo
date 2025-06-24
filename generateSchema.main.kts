938e79f92c5b0641f2080946ed300cbaf4f52c4c
Cương Nguyễn <cuong0993@gmail.com>
2025-06-24 08:49:22 +0700

    Update

--- generateSchema.main.kts
+++ generateSchema.main.kts
@@ -1,10 +1,10 @@
 #!/usr/bin/env kotlin
 @file:Repository("https://repo.maven.apache.org/maven2")
-@file:DependsOn("com.fasterxml.jackson.core:jackson-core:2.18.3")
-@file:DependsOn("com.graphql-java:graphql-java:22.3")
-@file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:3.1.1")
-@file:DependsOn("io.ktor:ktor-client-java-jvm:3.1.1")
-@file:DependsOn("io.ktor:ktor-serialization-jackson-jvm:3.1.1")
+@file:DependsOn("com.fasterxml.jackson.core:jackson-core:2.19.1")
+@file:DependsOn("com.graphql-java:graphql-java:24.1")
+@file:DependsOn("io.ktor:ktor-client-content-negotiation-jvm:3.2.0")
+@file:DependsOn("io.ktor:ktor-client-java-jvm:3.2.0")
+@file:DependsOn("io.ktor:ktor-serialization-jackson-jvm:3.2.0")
 
 import graphql.introspection.IntrospectionResultToSchema
 import graphql.schema.idl.SchemaPrinter
